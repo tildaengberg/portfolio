d11161cf8d99e52a3742775b5e61f659c490a278
tildaengberg <tildaengberg@hotmail.com>
2022-01-09 18:18:44 +0100

    tiny png, color changes

--- src/components/Projects_GarmAqua.js
+++ src/components/Projects_GarmAqua.js
@@ -10,33 +10,35 @@ import mockup from '../image/ga_mockup.png'
 function Projects_GarmAqua() {
     return (
         <div className='project'>
-        <div className='project_headline'>
+            <div className='project_headline'>
             </div>
 
             <div class="project_container">
                 <div class="project_wrapper">
-                <Link to="/projekt" className='close_btn'><i class="fas fa-times"></i></Link>
+                    <Link to="/projekt" className='close_btn'><i class="fas fa-times"></i></Link>
+
                     <ProjectInfo
                         headline='GarmAqua 
                         <span class="brmedium"></span> '
-                        
+
                         text='Mitt första riktiga projekt där mitt uppdrag var att utveckla en ny hemsida för företaget GarmAqua. Företaget ville även ha en ny grafisk profil, logotyp och reklamblad.
                         <span class="brmedium"></span> '
                         tool='Figma | React.js | Netlify | Adobe Illustrator'
                         src='../image/garmportfoio.png'
                     />
 
+
                     <div className='project_imgs'>
                         <div className='project_img1'>
-                        <img src={flyer} alt='profile-img'></img>
+                            <img src={flyer} alt='profile-img'></img>
 
                         </div>
                         <div className='project_img2'>
-                        <img src={logos} alt='profile-img'></img>
+                            <img src={logos} alt='profile-img'></img>
 
                         </div>
                         <div className='project_img3'>
-                        <img src={mockup} alt='profile-img'></img>
+                            <img src={mockup} alt='profile-img'></img>
                         </div>
 
                     </div>
